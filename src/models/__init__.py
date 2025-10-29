97663e1bfa6f35975e63306e6bee3c645363ddbe
Deva Kumar (AIML) <117890712+devasphn@users.noreply.github.com>
2025-10-29 23:55:19 +0530

    Update models __init__.py to include PublicHiFiGANVocoder

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,5 +3,6 @@
 from .hybrid_s2s import HybridS2SModel
 from .speech_tokenizer import SpeechTokenizer
 from .streaming_processor import StreamingProcessor
+from .hifigan_public import PublicHiFiGANVocoder
 
-__all__ = ["HybridS2SModel", "SpeechTokenizer", "StreamingProcessor"]
+__all__ = ["HybridS2SModel", "SpeechTokenizer", "StreamingProcessor", "PublicHiFiGANVocoder"]
